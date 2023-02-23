5d664598e03f8d01ba2dbdfbe572fc893677b441
seans <sstclair@uvm.edu>
2023-02-22 20:08:41 -0800

    Added getters and setters

--- main.cpp
+++ main.cpp
@@ -5,8 +5,18 @@
 using namespace std;
 
 int main() {
+    // Create shuffle vectors
     vector<int> v1 = {1, 2, 3, 4, 7};
-    ShuffleVector<int> vec(v1);
-    vec.sortVector();
+    ShuffleVector<int> shuffleVec(v1);
+    shuffleVec.sortVector();
+
+    // Test randomness
+    shuffleVec.randomizeSstclair();
+    testRelativeFrequency(shuffleVec);
+    testRelativeFrequencePerIndex(shuffleVec);
+    testConsecutiveIdenticalItems(shuffleVec);
+    testMaxConsecutiveIndenticalItems(shuffleVec);
+
+    shuffleVec.randomizeLpiloco();
     return 0;
 }
