6ce2d591deb9c24af7240bf7bfe23bb91d3fadb4
Skeasty <lpiloco@uvm.edu>
2023-02-26 21:42:36 -0500

    randomizeLpiloco and consecutive test done

--- main.cpp
+++ main.cpp
@@ -28,36 +28,39 @@ int main() {
 
     // Demonstrate sorting (before shuffling)
     cout << "Shuffle Vector before sorting: " << endl;
-    cout << svec << endl;
+    cout << svec << endl << endl;
     cout << "Shuffle Vector after sorting: " << endl;
     svec.sortVector();
-    cout << svec << endl;
+    cout << svec << endl << endl;
 
     // Shuffles according to first algorithm (static seed)
     svec.randomizeSstclair(0);
     cout << "Shuffle Vector after randomizeSstclair with static seed: " << endl;
-    cout << svec << endl;
+    cout << svec << endl << endl;
     cout << "Shuffle Vector after sorting: " << endl;
     svec.sortVector();
-    cout << svec << endl;
+    cout << svec << endl << endl;
 
     // Shuffles according to first algorithm (dynamic seed)
     svec.randomizeSstclair(time(0));
     cout << "Shuffle Vector after randomizeSstclair with dynamic seed: " << endl;
-    cout << svec << endl;
+    cout << svec << endl << endl;
     cout << "Shuffle Vector after sorting: " << endl;
     svec.sortVector();
-    cout << svec << endl;
+    cout << svec << endl << endl;
 
     // Shuffles according to second algorithm
     svec.randomizeLpiloco();
     cout << "Shuffle Vector after randomizeLpiloco: " << endl;
-    cout << svec << endl;
+    cout << svec << endl << endl;
     cout << "Shuffle Vector after sorting: " << endl;
     svec.sortVector();
-    cout << svec << endl;
+    cout << svec << endl << endl;
 
+    cout << "Relative Frequency Test:" << endl;
     testRelativeFrequencyPerIndex(svec);
+
+    cout << "Consecutive Items Test:" << endl;
     testConsecutiveIdenticalItems(svec);
     return 0;
 }
@@ -81,19 +84,50 @@ void testRelativeFrequencyPerIndex(ShuffleVector<T> &vec) {
 template<typename T>
 void testConsecutiveIdenticalItems(ShuffleVector<T> &vec) {
     vector<T> originalVector = vec.getVector();
-    vector<T> consecutiveItems = {};
+    vector<T> consecutiveItemsL = {};
     vec.randomizeLpiloco();
-    vector<T> newVector = vec.getVector();
+    vector<T> newVectorL = vec.getVector();
 
-    int counter = 0;
+    vec.sortVector();
+    vector<T> consecutiveItemsS = {};
+    vec.randomizeSstclair(2);
+    vector<T> newVectorS = vec.getVector();
+
+    int counterL = 0;
+    int counterS = 0;
     for (int i = 0; i < size(originalVector) - 1; ++i) {
         for (int j = 0; j < size(originalVector) - 1; ++j) {
-            if (newVector[i] == originalVector[j] && newVector[i + 1] == originalVector[j + 1]) {
-                consecutiveItems.push_back(newVector[i]);
-                consecutiveItems.push_back(newVector[i + 1]);
-                ++counter;
+            if (newVectorL[i] == originalVector[j] && newVectorL[i + 1] == originalVector[j + 1]) {
+                consecutiveItemsL.push_back(newVectorL[i]);
+                consecutiveItemsL.push_back(newVectorL[i + 1]);
+                ++counterL;
+            }
+            if (newVectorS[i] == originalVector[j] && newVectorS[i + 1] == originalVector[j + 1]) {
+                consecutiveItemsS.push_back(newVectorS[i]);
+                consecutiveItemsS.push_back(newVectorS[i + 1]);
+                ++counterS;
             }
         }
     }
-    cout << "Consecutive values: " << counter << endl;
+
+    int currentMax = 0;
+    int tempCount = 2;
+    for (int i = 1; i < size(consecutiveItemsL); ++i) {
+        if (tempCount > currentMax && tempCount > 2) {
+            currentMax = tempCount;
+        }
+
+        if (consecutiveItemsL[i] == consecutiveItemsL[i + 1]) {
+            ++tempCount;
+        }
+        else {
+            tempCount = 2;
+        }
+    }
+
+    cout << "Consecutive pairs in randomizeLpiloco: " << counterL << endl;
+    cout << "Length of longest consecutive items: " << currentMax << endl << endl;
+
+    cout << "Consecutive pairs in randomizeSstclair: " << counterS << endl;
+    cout << "Length of longest consecutive items: " << endl;
 }
